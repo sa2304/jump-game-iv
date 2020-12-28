e0e5732defe3cb0f73df114a7f8963308bba6572
Антон Шмидт <schmidt.ajax@gmail.com>
2020-12-28 19:31:27 +0700

    Use vector instead of set

--- solution.h
+++ solution.h
@@ -14,18 +14,18 @@ public:
   int minJumps(std::vector<int>& arr) {
     using Index = int;
     using Value = int;
-    std::unordered_map<Value, std::set<Index>> jumps;
+    std::unordered_map<Value, std::vector<Index>> jumps;
     int n = arr.size();
     for (size_t i = 0; i < n; ++i) {
-      jumps[arr[i]].insert(i);
+      jumps[arr[i]].push_back(i);
     }
 
     std::queue<Index> bfs_queue;
-    std::set<Index> visited;
+    std::vector<bool> visited(n, false);
     int level = 0;
 
     bfs_queue.push(0);
-    visited.insert(0);
+    visited[0] = true;
 
     while (!bfs_queue.empty()) {
       int level_nodes_count = bfs_queue.size();
@@ -37,21 +37,21 @@ public:
         }
 
         Index prev_index = index - 1;
-        if (0 <= prev_index && !visited.count(prev_index)) {
+        if (0 <= prev_index && !visited[prev_index]) {
           bfs_queue.push(prev_index);
-          visited.insert(prev_index);
+          visited[prev_index] = true;
         }
 
         Index next_index = index + 1;
-        if (next_index < n && !visited.count(next_index)) {
+        if (next_index < n && !visited[next_index]) {
           bfs_queue.push(next_index);
-          visited.insert(next_index);
+          visited[next_index] = true;
         }
 
         for (Index jump_index : jumps.at(arr[index])) {
-          if (!visited.count(jump_index)) {
+          if (!visited[jump_index]) {
             bfs_queue.push(jump_index);
-            visited.insert(jump_index);
+            visited[jump_index] = true;
           }
         }
         jumps[arr[index]].clear();
